58dc4ecb4dd646d16c62ca58514c818b2f907e31
hamed <amzhamed@gmail.com>
2023-06-20 10:18:35 -0700

    experiment

--- test_config.py
+++ test_config.py
@@ -2,7 +2,7 @@ class TestConfig:
     ENABLED = True
     NUMBER_OF_FLSS = 60
     K = 5
-    R = 10  # ratio of r2 to r1
+    R = 1  # ratio of r2 to r1
     H = 2  # heuristic 1, 2, vns, rs
     ETA = K - 1  # initial eta only effective for h:1 and h:rs
     ETA_STR = "k-1"
