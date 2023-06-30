874bc51437784a507ceb4f5efcaa163fa0d716b0
hamed <amzhamed@gmail.com>
2023-06-30 09:08:41 -0700

    fix

--- start_cluster.sh
+++ start_cluster.sh
@@ -6,7 +6,7 @@ now=$(date +%d-%b-%H_%M_%S)
 
 for (( i=1; i<num_of_total_servers; i++ )); do
     server_addr=${USERNAME}@node-$i.${HOSTNAME}
-    ssh -oStrictHostKeyChecking=no -f "${server_addr}" "sleep 20 && ulimit -n 9999 && cd SwarMer && nohup python3 server.py ${num_of_total_servers} ${i} ${now} > my.log 2>&1 &" &
+    ssh -oStrictHostKeyChecking=no -f "${server_addr}" "sleep 10 && ulimit -n 9999 && cd SwarMer && nohup python3 server.py ${num_of_total_servers} ${i} ${now} > my.log 2>&1 &" &
 done
 
-ulimit -n 9999 && cd SwarMer && python3 server.py "${num_of_total_servers}" 0 "${now}"
+ulimit -n 9999 && python3 server.py "${num_of_total_servers}" 0 "${now}"
