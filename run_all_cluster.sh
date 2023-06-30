f4f8dadd0dda795142a762fb3a6d642f506e7f02
hamed <amzhamed@gmail.com>
2023-06-30 10:02:40 -0700

    fix

--- run_all_cluster.sh
+++ run_all_cluster.sh
@@ -5,7 +5,7 @@ sleep 10
 
 for i in {0..3}
 do
-   bash set_conf_cluster.sh "$i"
-   sleep 10
-   bash start_cluster.sh
+#   bash set_conf_cluster.sh "$i"
+#   sleep 10
+   bash start_cluster.sh "$i"
 done
