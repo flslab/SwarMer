1dc26387b0e27a65439c5f5babc8bfc152093acf
hamed <amzhamed@gmail.com>
2023-07-08 21:14:35 -0700

    experiment

--- server.py
+++ server.py
@@ -240,9 +240,9 @@ if __name__ == '__main__':
             np.random.shuffle(point_cloud)
             point_cloud = point_cloud[:Config.SAMPLE_SIZE]
 
-    # dists = np.linalg.norm(point_cloud, axis=1)
-    # sorted_indexes = np.flip(np.argsort(dists))
-    # point_cloud = point_cloud[sorted_indexes]
+    dists = np.linalg.norm(point_cloud, axis=1)
+    sorted_indexes = np.flip(np.argsort(dists))
+    point_cloud = point_cloud[sorted_indexes]
 
     total_count = point_cloud.shape[0]
     # h = np.log2(total_count)
