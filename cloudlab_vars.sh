df044d2de4d181f4a7f5820552f171a488a609cb
hamed <amzhamed@gmail.com>
2023-07-11 14:13:01 -0700

    fix scripts

--- cloudlab_vars.sh
+++ cloudlab_vars.sh
@@ -2,5 +2,6 @@
 
 idx=1
 num_of_total_servers=16
+N=15
 HOSTNAME="reli-group.nova-PG0.clemson.cloudlab.us"
 USERNAME="hamedamz"
--- start_cluster.sh
+++ start_cluster.sh
@@ -6,7 +6,7 @@ now=$(date +%d-%b-%H_%M_%S)
 
 for (( i=1; i<num_of_total_servers; i++ )); do
     server_addr=${USERNAME}@node-$i.${HOSTNAME}
-    ssh -oStrictHostKeyChecking=no -f "${server_addr}" "ulimit -n 99999 && cd SwarMer && cp experiments/config$1.py config.py && sleep 10 && nohup python3 server.py ${num_of_total_servers} ${i} ${now} > my.log 2>&1 &" &
+    ssh -oStrictHostKeyChecking=no -f "${server_addr}" "ulimit -n 99999 && cd SwarMer && cp experiments/config$1.py config.py && sleep 10 && nohup python3 server.py ${num_of_total_servers} ${now} > my.log 2>&1 &" &
 done
 
-ulimit -n 99999 && cp "experiments/config$1.py" config.py && sleep 1 && python3 server.py "${num_of_total_servers}" 0 "${now}"
+ulimit -n 99999 && cp "experiments/config$1.py" config.py && sleep 1 && python3 server.py "${N}" "${now}"
