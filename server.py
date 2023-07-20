eb97aa420adf6c52db5d6063a13c176fa5d55256
hamed <amzhamed@gmail.com>
2023-07-19 18:55:20 -0700

    experiment

--- server.py
+++ server.py
@@ -401,7 +401,7 @@ if __name__ == '__main__':
                 # print(connections)
                 break
 
-            time.sleep(.05)
+            time.sleep(1)
 
     if nid == 0:
         stop.stop_all()
