7b24d00d1d6915dff39a0205c2f55296eaab4162
hamed <amzhamed@gmail.com>
2023-08-09 21:27:01 -0700

    add rs prop plot

--- utils/plot.py
+++ utils/plot.py
@@ -9,7 +9,6 @@ import scipy
 from matplotlib import rcParams, pylab
 from matplotlib import ticker
 
-rcParams['font.family'] = 'Times New Roman'
 
 # mpl.use('macosx')
 
@@ -74,6 +73,74 @@ def quad():
     plt.savefig(f'/Users/hamed/Desktop/4_shapes.png', dpi=300)
 
 
+def p_rs(F, G, eta):
+    return math.comb(F - 1 - (G - 1), eta - (G - 1)) / math.comb(F - 1, eta)
+
+
+def rs_probability():
+    fig = plt.figure(figsize=[4, 3])
+    ax = fig.add_subplot()
+
+    # RS probability, SZ=G-1, y-axis is the P, x-axis is G 2, to 10 or 15
+    xs = [2, 3, 5, 6, 7, 8, 9, 10]
+    ys_30 = [p_rs(F=30, G=g, eta=g-1) for g in xs]
+    ys_60 = [p_rs(F=60, G=g, eta=g-1) for g in xs]
+    ys_90 = [p_rs(F=90, G=g, eta=g-1) for g in xs]
+
+    ax.plot(xs, ys_30)
+    ax.plot(xs, ys_60)
+    ax.plot(xs, ys_90, color='tab:purple')
+    plt.text(xs[-1]-1, ys_30[-1]*4, 'F=30', color='tab:blue')
+    plt.text(xs[-1]-1, ys_60[-1]*8, 'F=60', color='tab:orange')
+    plt.text(xs[-1]-1, ys_90[-1]*10, 'F=90', color='tab:purple')
+    ax.set_yscale('log')
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.set_ylim([1e-13, 1])
+    ax.set_ylabel('Probability', loc='top', rotation=0, labelpad=-50)
+    ax.set_xlabel('G', loc='right')
+    ax.spines['top'].set_color('white')
+    ax.spines['right'].set_color('white')
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig('/Users/hamed/Desktop/rs_prop_f30_f60_f90_g.png', dpi=300)
+
+
+def rs_probability_2():
+    fig = plt.figure(figsize=[4, 3])
+    ax = fig.add_subplot()
+
+    # RS probability, SZ=G-1, y-axis is the P, x-axis is G 2, to 10 or 15
+    xs = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    ys_3 = [p_rs(F=f, G=3, eta=3-1) for f in xs]
+    ys_5 = [p_rs(F=f, G=5, eta=5-1) for f in xs]
+    ys_10 = [p_rs(F=f, G=10, eta=10-1) for f in xs]
+
+    ax.plot(xs, ys_3)
+    ax.plot(xs, ys_5)
+    ax.plot(xs, ys_10, color='tab:purple')
+    plt.text(xs[-1] - 10, ys_3[-1] * 4, 'G=3', color='tab:blue')
+    plt.text(xs[-1] - 10, ys_5[-1] * 5, 'G=5', color='tab:orange')
+    plt.text(xs[-1] - 10, ys_10[-1] * 7, 'G=10', color='tab:purple')
+    ax.set_yscale('log')
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(10))
+    # ax.yaxis.set_major_locator(ticker.MultipleLocator(lambda x: x*10))
+
+    # y_formatter = ticker.FixedFormatter(["-1e7", "111", "007", "xkcd"])
+    y_locator = ticker.FixedLocator([1e-13, 1e-11, 1e-9, 1e-7, 1e-5, 1e-3, 1e-1, 1])
+    # ax.yaxis.set_major_formatter(y_formatter)
+    ax.yaxis.set_major_locator(y_locator)
+
+    ax.set_ylim([1e-13, 10])
+    ax.set_ylabel('Probability', loc='top', rotation=0, labelpad=-50)
+    ax.set_xlabel('F', loc='right')
+    ax.spines['top'].set_color('white')
+    ax.spines['right'].set_color('white')
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig('/Users/hamed/Desktop/rs_prop_g3_g5_g10_f.png', dpi=300)
+
+
 if __name__ == '__main__':
+    rcParams['font.family'] = 'Times New Roman'
     mpl.use('macosx')
-    quad()
+    rs_probability_2()
